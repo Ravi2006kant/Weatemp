5a3c3700389e1e2628ddab3521847129121f8978
Ravi <ravi5gangster@gmail.com>
2026-06-05 11:47:02 +0530

    basic layou

--- lib/components/botnav.dart
+++ lib/components/botnav.dart
@@ -4,23 +4,20 @@ class Botnavigation extends StatelessWidget {
   final int index;
   final void Function(int)? ontap;
 
-  const Botnavigation({required this.index,required this.ontap});
+  const Botnavigation({required this.index, required this.ontap});
 
   @override
   Widget build(BuildContext context) {
     return BottomNavigationBar(
       currentIndex: index,
-      onTap:ontap,
+      onTap: ontap,
       type: BottomNavigationBarType.fixed,
-      showSelectedLabels: true,
       backgroundColor: Theme.of(context).colorScheme.primary,
-      selectedLabelStyle: TextStyle(
-        color: Theme.of(context).colorScheme.secondary,
-        fontWeight: .bold,
-      ),
-      unselectedLabelStyle: TextStyle(
-        color: Theme.of(context).colorScheme.secondary,
-      ),
+      showSelectedLabels: true,
+      selectedItemColor: Colors.white,
+      unselectedItemColor: Colors.white,
+      selectedLabelStyle: TextStyle(color: Colors.white, fontWeight: .bold),
+      unselectedLabelStyle: TextStyle(color: Colors.white),
       items: [
         BottomNavigationBarItem(
           icon: Icon(
